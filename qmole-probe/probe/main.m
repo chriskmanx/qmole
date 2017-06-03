aa1ee0e6b72cc00f5b27b4bfeb3bf96e191df1ef
Chris Kohlhepp <chrisk@manx.net>
2017-06-03 22:25:50 +1000

    Add QMole probe

---
+++ qmole-probe/probe/main.m
@@ -0,0 +1,33 @@
+//
+//  main.m
+//  probe
+//
+//  Created by Chris Kohlhepp on 2/05/2015.
+//  Copyright (c) 2015 Chris Kohlhepp. All rights reserved.
+//
+
+
+#import <sys/sysctl.h>
+
+
+int main(int argc, char * argv[])
+{
+    NSLog(@"Running");
+    size_t size;
+    sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+    char *machine = malloc(size);
+    sysctlbyname("hw.machine", machine, &size, NULL, 0);
+    NSString *platform = [NSString stringWithCString:machine encoding:NSUTF8StringEncoding];
+    NSLog(@"Device %@",platform);
+    
+    
+    //NSString *udid = [[[UIDevice currentDevice]identifierForVendor]UUIDString];
+    //UIDevice *myDevice = [UIDevice currentDevice];
+    
+    //NSString *deviceOSVersion = myDevice.systemVersion;
+    
+    //NSLog(@"OS Version %@",deviceOSVersion);
+    
+    free(machine);
+    return 0;
+}
---
+++ qmole-probe/probeTests/probeTests.m
@@ -0,0 +1,34 @@
+//
+//  probeTests.m
+//  probeTests
+//
+//  Created by Chris Kohlhepp on 2/05/2015.
+//  Copyright (c) 2015 Chris Kohlhepp. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface probeTests : XCTestCase
+
+@end
+
+@implementation probeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
